9aef70a84c17e4fdfd6fa601b8ea89117f237824
SANTHOSH KUMAR GOWDA <gowda.santhu1939@gmail.com>
2025-04-01 20:19:28 -0400

    Add files via upload

---
+++ iterative statments.py
@@ -0,0 +1,108 @@
+# Coding Exercise:
+# Exercise 1: Sum of Squares
+# Write a Python program that calculates and prints the sum of the squares of
+# numbers from 1 to 5 using a 
+# for loop.
+ 
+# for a in [1,2,3,4,5]:
+#     print(f"sum of squares {a} is {a*a}")
+
+# output:
+# sum of squares 1 is 1
+# sum of squares 2 is 4
+# sum of squares 3 is 9
+# sum of squares 4 is 16
+# sum of squares 5 is 25
+
+
+
+
+# Exercise 2: Countdown
+# Write a Python program that uses a 
+# while loop to print a countdown from 5 to 1.
+# i=0
+# while i in range(5,1):
+#                print(f"count down {i}")
+
+# Exercise 3: Multiplication Table with Nested For Loop
+# Write a Python program to print the multiplication table for a user-specified
+# number using a nested for loop.
+
+# number=int(input("Enter a number:"))
+# for i in range(1,11):
+#     print(f"{number} x {i} = {number*i}")
+
+# output:
+# Enter a number:20
+# 20 x 1 = 20
+# 20 x 2 = 40
+# 20 x 3 = 60
+# 20 x 4 = 80
+# 20 x 5 = 100
+# 20 x 6 = 120
+# 20 x 7 = 140
+# 20 x 8 = 160
+# 20 x 9 = 180
+# 20 x 10 = 200
+
+
+# Exercise 4:
+# Write a Python program that uses a "for" loop to find the sum of all even
+# numbers between 0 and 10 (inclusive).
+
+# n=0
+# for i in range(0,11,2):
+#     n += i
+#     print(f"sum of even numbers 0 to {i} : {n}")
+
+# output:
+# sum of even numbers 0 to 0 : 0
+# sum of even numbers 0 to 2 : 2
+# sum of even numbers 0 to 4 : 6
+# sum of even numbers 0 to 6 : 12
+# sum of even numbers 0 to 8 : 20
+# sum of even numbers 0 to 10 : 30
+
+
+# Exercise 5:
+# Calculate the sum of all numbers from 1 to a given number
+# a=int(input("enter a number : "))
+# i=int(input("enter another number :"))
+# for i in range(0,999,1):
+#     a =+ i
+#     print(f"sum of all numbers {a+i}")
+
+
+# Exercise 6:
+# Display numbers from a list using loop
+# for i in [1,2,3,4,5,6,7]:
+#     print(i)
+
+# output:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+
+
+
+
+
+
+# Exercise 7:
+# Display numbers from -10 to -1 using for loop
+
+# for i in range(-10,0,1):
+#     print(i)
+
+# Exercise 8:
+# Write a program to display all prime numbers within a range
+# n=int(input("enter a number :"))
+# for i in range(0,15):
+#     if i == n%1==1 and n%n==1:
+#         print(i)
+    
+
